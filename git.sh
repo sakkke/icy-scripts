337061ee6f036d3368a4aac4feca8c68979a7acb
sakkke <w32w64@gmail.com>
2022-07-23 08:46:13 +0000

    add `git.sh`

---
+++ git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+./serve.sh &
+serve_pid=$!
+
+while true; do
+	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
+		break
+	fi
+
+	sleep 1
+done
+
+./setup.sh
+./setup-dev.sh
+
+kill -s KILL $serve_pid
+
+./setup-git.sh
