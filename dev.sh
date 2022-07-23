b6437ea086d07d14395791ed1d9cfd9d65f1e725
sakkke <w32w64@gmail.com>
2022-07-23 05:02:43 +0000

    add `dev.sh`

---
+++ dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eux
+
+device="$1"
+
+umount --recursive /mnt || true
+
+./serve.sh &
+serve_pid=$!
+
+while true; do
+	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
+		break
+	fi
+
+	sleep 1
+done
+
+./setup.sh
+./mount.sh "$device"
+./pacstrap.sh
+
+kill -s KILL $serve_pid
+
+./passwd.sh
+./grub-install.sh
