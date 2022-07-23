188e23a7df86e19d9582e4f1ae8262bc4eb9deab
sakkke <w32w64@gmail.com>
2022-07-23 09:10:41 +0000

    disable progress bar in `pacstrap`

--- pacstrap.sh
+++ pacstrap.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-pacstrap /mnt base base-devel dkms efibootmgr gnome grub linux-firmware linux-zen linux-zen-headers networkmanager noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra paru-bin vi vim
+pacstrap /mnt --noprogressbar base base-devel dkms efibootmgr gnome grub linux-firmware linux-zen linux-zen-headers networkmanager noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra paru-bin vi vim
