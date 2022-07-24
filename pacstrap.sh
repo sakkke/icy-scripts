91b561d3d543b076b9b3832dd1dab2503f397641
sakkke <w32w64@gmail.com>
2022-07-24 02:59:57 +0000

    split package list into separate file

--- pacstrap.sh
+++ pacstrap.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-pacstrap -C /icy/pacman.conf /mnt --color auto --noprogressbar base base-devel dkms efibootmgr gnome grub linux-firmware linux-zen linux-zen-headers networkmanager noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra paru-bin vi vim
+cat packages.x86_64 | pacstrap -C /icy/pacman.conf /mnt --color auto --noprogressbar -
