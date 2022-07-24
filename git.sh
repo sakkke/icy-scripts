75922ff0618c0f28a8ed4d521357eb4f1e248bd4
sakkke <w32w64@gmail.com>
2022-07-24 05:58:15 +0000

    refactor code

--- git.sh
+++ git.sh
@@ -2,19 +2,5 @@
 
 set -eux
 
-./serve.sh &
-serve_pid=$!
-
-while true; do
-	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
-		break
-	fi
-
-	sleep 1
-done
-
 ./setup-dev.sh
-
-kill -s KILL $serve_pid
-
 ./setup-git.sh
--- pacman.sh
+++ pacman.sh
@@ -2,6 +2,19 @@
 
 set -eux
 
+./serve.sh &
+serve_pid=$!
+
+while true; do
+	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
+		break
+	fi
+
+	sleep 1
+done
+
 mount -o remount,size=1G /run/archiso/cowspace
 pacman --config /icy/pacman.conf -Sy
 pacman --config /icy/pacman.conf --needed --noconfirm "$@"
+
+kill -s KILL $serve_pid
