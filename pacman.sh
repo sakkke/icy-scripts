f198faa33560f9c0291ab240a94843a680c09b9c
sakkke <w32w64@gmail.com>
2022-07-24 08:52:22 +0000

    disable progress bar in `pacman`

--- pacman.sh
+++ pacman.sh
@@ -14,7 +14,7 @@ while true; do
 done
 
 mount -o remount,size=1G /run/archiso/cowspace
-pacman --config /icy/pacman.conf -Sy
-pacman --config /icy/pacman.conf --needed --noconfirm "$@"
+pacman --config /icy/pacman.conf --noprogressbar -Sy
+pacman --config /icy/pacman.conf --needed --noconfirm --noprogressbar "$@"
 
 kill -s KILL $serve_pid
