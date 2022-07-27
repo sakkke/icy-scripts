4494c4afd477eff7fe094139c25b97745e1533a7
sakkke <w32w64@gmail.com>
2022-07-27 12:56:19 +0000

    make `makepkg` to use custom `pacman.conf`

--- makepkg.sh
+++ makepkg.sh
@@ -2,5 +2,20 @@
 
 set -eux
 
+cwd=$(cd "$(dirname "$0")" && pwd)
+
+"$cwd/serve.sh" &
+serve_pid=$!
+
+while true; do
+	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
+		break
+	fi
+
+	sleep 1
+done
+
 chown -R user:user .
 sudo -u user makepkg --noconfirm -s
+
+kill -s KILL $serve_pid
