2598d86dbcee359703176a6a8bde942689aecc14
sakkke <w32w64@gmail.com>
2022-07-29 11:31:07 +0000

    add `catppuccin-wallpapers-git`

--- icy/icy/os/x86_64/sync.sh
+++ icy/icy/os/x86_64/sync.sh
@@ -2,6 +2,7 @@
 
 set -eux
 
+git clone https://aur.archlinux.org/catppuccin-wallpapers-git.git || true
 git clone https://aur.archlinux.org/gnome-shell-extension-material-shell-git.git || true
 git clone https://aur.archlinux.org/linux-xanmod.git || true
 git clone https://aur.archlinux.org/paru-bin.git || true
