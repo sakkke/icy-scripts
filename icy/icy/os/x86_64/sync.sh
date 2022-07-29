dd2f2dff3eb678170d8f9033c4f6f1322e104a16
sakkke <w32w64@gmail.com>
2022-07-29 10:48:31 +0000

    add `gnome-shell-extension-material-shell-git`

--- icy/icy/os/x86_64/sync.sh
+++ icy/icy/os/x86_64/sync.sh
@@ -2,5 +2,6 @@
 
 set -eux
 
+git clone https://aur.archlinux.org/gnome-shell-extension-material-shell-git.git || true
 git clone https://aur.archlinux.org/linux-xanmod.git || true
 git clone https://aur.archlinux.org/paru-bin.git || true
