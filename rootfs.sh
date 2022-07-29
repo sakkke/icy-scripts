63b0bf8699538e123fd142d957b69db0945de040
sakkke <w32w64@gmail.com>
2022-07-29 11:24:46 +0000

    make `gzip` `pigz`

--- rootfs.sh
+++ rootfs.sh
@@ -5,5 +5,5 @@ set -eux
 cd /mnt
 mkdir -p /icy/build
 out=/icy/build/rootfs-$(date +%s).tar.gz
-tar --use-compress-program='gzip -9' -cf $out *
+tar --use-compress-program='pigz --best' -cf $out *
 ln -fs $out /icy/build/rootfs-latest.tar.gz
