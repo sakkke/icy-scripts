fc3166b14d8b6464d2263cf94ea4de6f5b93bd48
sakkke <w32w64@gmail.com>
2022-07-31 23:18:03 +0000

    add Victor Mono

--- icy/icy/os/x86_64/sync.sh
+++ icy/icy/os/x86_64/sync.sh
@@ -4,6 +4,7 @@ set -eux
 
 git clone https://aur.archlinux.org/catppuccin-gtk-theme.git || true
 git clone https://aur.archlinux.org/catppuccin-wallpapers-git.git || true
+git clone https://aur.archlinux.org/font-victor-mono.git || true
 git clone https://aur.archlinux.org/gnome-shell-extension-material-shell-git.git || true
 git clone https://aur.archlinux.org/linux-xanmod.git || true
 git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git || true
