7b9c1898d07c2861b5df94d6dcba8a6a5db4f539
sakkke <w32w64@gmail.com>
2022-07-31 01:46:07 +0000

    add `pv`

--- rootfs.sh
+++ rootfs.sh
@@ -4,6 +4,8 @@ set -eux
 
 cd /mnt
 mkdir -p /icy/build
-out=/icy/build/rootfs-$(date +%s).tar.gz
-tar --use-compress-program='pigz --best' -cf $out *
-ln -fs $out /icy/build/rootfs-latest.tar.gz
+out=/icy/build/rootfs-$(date +%s).tar
+tar -cf - * | pv > $out
+pv $out | pigz --best > $out.gz
+ln -fs $out /icy/build/rootfs-latest.tar
+ln -fs $out.gz /icy/build/rootfs-latest.tar.gz
