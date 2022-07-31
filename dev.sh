2ff51a8aa44cebda29761e25411996d96ab5f816
sakkke <w32w64@gmail.com>
2022-07-31 02:53:46 +0000

    refactor code

--- dev.sh
+++ dev.sh
@@ -10,18 +10,6 @@ if [[ -f /icy/build/rootfs-latest.tar ]]; then
 	./mount.sh "$device"
 	./extract-rootfs.sh
 else
-	./serve.sh &
-	serve_pid=$!
-	trap 'kill -s KILL $serve_pid' EXIT
-
-	while true; do
-		if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
-			break
-		fi
-
-		sleep 1
-	done
-
 	./mount.sh "$device"
 	./pacstrap.sh /mnt - < packages.x86_64
 
--- pacstrap.sh
+++ pacstrap.sh
@@ -2,4 +2,16 @@
 
 set -eux
 
+./serve.sh &
+serve_pid=$!
+trap 'kill -s KILL $serve_pid' EXIT
+
+while true; do
+	if curl --max-time 2 --output /dev/null http://localhost:8080/sync.sh; then
+		break
+	fi
+
+	sleep 1
+done
+
 pacstrap -C /icy/pacman.conf "$@"
