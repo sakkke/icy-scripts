a41bffe771d015600ccd148a20a8d91adc2506e6
sakkke <w32w64@gmail.com>
2022-07-31 22:54:22 +0000

    change Firefox to Microsoft Edge

--- icy/icy/os/x86_64/sync.sh
+++ icy/icy/os/x86_64/sync.sh
@@ -6,5 +6,6 @@ git clone https://aur.archlinux.org/catppuccin-gtk-theme.git || true
 git clone https://aur.archlinux.org/catppuccin-wallpapers-git.git || true
 git clone https://aur.archlinux.org/gnome-shell-extension-material-shell-git.git || true
 git clone https://aur.archlinux.org/linux-xanmod.git || true
+git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git || true
 git clone https://aur.archlinux.org/paru-bin.git || true
 git clone https://aur.archlinux.org/tabby-bin.git || true
